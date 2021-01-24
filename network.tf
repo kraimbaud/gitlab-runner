8dcfc892bedd61a0876cf6b4e064cf713b8e690e
Kevin <kraim@free.fr>
2021-01-24 23:03:14 +0100

    allow ssh connexion

--- network.tf
+++ network.tf
@@ -2,4 +2,14 @@ resource "google_compute_network" "vpc_network" {
   name = var.network
 }
 
+resource "google_compute_firewall" "ssh-rule" {
+  name = "ssh-connexion"
+  network = google_compute_network.vpc_network.name
 
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
